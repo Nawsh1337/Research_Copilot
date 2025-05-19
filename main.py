e9f6a5582dc6632dd13536c03a4febfb1a2b3bae
Nawshrvan Arshad <nawshrvanarshad@gmail.com>
2025-05-19 22:14:41 +0200

    Implement multiple code changes to enhance functionality and improve performance

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from research-helper-langchain!")
+import streamlit as st
+import parser
+import tempfile
+from pathlib import Path
 
+st.set_page_config(page_title="Research Copilot", layout="centered")
+st.title("📄 Research Copilot: PDF Reader")
 
-if __name__ == "__main__":
-    main()
+uploaded_file = st.file_uploader("Upload a PDF", type=["pdf"])
+if uploaded_file is not None:
+    with st.spinner("Processing..."):
+        # print(uploaded_file.name)
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:#creating temp file so that the path is given fully
+            tmp.write(uploaded_file.read())
+            tmp_path = Path(tmp.name)
+        full_text, page_texts, metadata = parser.extract_text_from_pdf(tmp_path,uploaded_file.name)
+        chunks = parser.chunk_pages_with_metadata(page_texts)
+
+        # st.subheader("Extracted Text")
+        st.subheader("Metadata")
+        st.json(metadata)
+
+        st.subheader(f"Total Chunks: {len(chunks)}")
+        for c in chunks[:3]:#show first 3 chunks
+            st.markdown(f"**Page {c['page']}**:")
+            st.write(c['text'])
+
+        # question = st.text_input("Ask a question about the text:")
+        # if question:
+        #     with st.spinner("Generating answer..."):
+        #         answer = parser.answer_question(pdf_text, question)
+        #         st.subheader("Answer")
+        #         st.write(answer)
+
---
+++ parser.py
@@ -0,0 +1,34 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import fitz #PyMuPDF
+from pathlib import Path
+
+def extract_text_from_pdf(pdf_path,file_name):
+    doc = fitz.open(str(pdf_path))
+    page_texts = []
+
+    for page in doc:
+        page_texts.append(page.get_text())
+    full_text = "\n\n".join(page_texts)
+    metadata = {
+        "file_name": file_name,
+        "num_pages": len(doc),
+        "title": doc.metadata.get("title") or "Unknown",
+        "author": doc.metadata.get("author") or "Unknown",
+    }
+
+    doc.close()
+    return full_text,page_texts, metadata
+
+def chunk_pages_with_metadata(page_texts):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = []
+
+    for i, page_text in enumerate(page_texts, start=1):
+        page_chunks = splitter.split_text(page_text)
+        for chunk in page_chunks:
+            chunks.append({
+                "page": i,
+                "text": chunk
+            })
+
+    return chunks
